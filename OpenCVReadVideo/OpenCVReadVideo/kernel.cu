8270a3e517ce6a736bfb32920a6c159126393669
truenicfel <dassler@hm.edu>
2018-05-04 17:49:57 +0200

    Implemented that two channels are zero

--- OpenCVReadVideo/OpenCVReadVideo/kernel.cu
+++ OpenCVReadVideo/OpenCVReadVideo/kernel.cu
@@ -4,4 +4,21 @@
 
 __global__ void addKernel(char* outputMatrix, char* inputMatrix, int rows, int columns)
 {
+  // which pixel does this thread have to work on?
+	int column = blockIdx.x*blockDim.x + threadIdx.x;
+  int row = blockIdx.y*blockDim.y + threadIdx.y;
+  
+  if ((column < columns) && (row < rows)) {
+    // each element has size 3 bytes (provide as param?)
+    // calculate offset to access correct element
+    int offset = (column * 3)             // offset in a row
+                + (columns * 3 * row);    // select row
+    // copy one value to the result matrix and set other 2 to zero
+    // first channel (r, g or b? would be interesting to know)
+    outputMatrix[offset] = inputMatrix[offset];
+    // second channel
+    outputMatrix[offset + 1] = 0;
+    // third channel
+    outputMatrix[offset + 2] = 0;
+  }
 }
